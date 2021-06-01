38f4e5c8667bdb3cc76074139202f3493e7438a8
pierre averous <pierreaverous42@gmail.com>
2021-06-01 23:52:43 +0200

    views profile user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :reservation
   validates :rating, number: { in:  1..5 }, presence: :true
-  validates :content, length:  { in: 6..200 }, presence: :true
+  validates :content, length: { in: 6..200 }, presence: :true
 end
