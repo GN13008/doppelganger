d406811889b5b6402d272d875dc8bd51f78236f0
pierre averous <pierreaverous42@gmail.com>
2021-06-01 06:48:27 +0200

    review mvc create model and controller methode create and notice & alert

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
   def create
-  end
+    @review = Review.new(review_params)
+    @reservation = Reservation.find(params[:reservation_id])
+    @review.reservation = @reservation
+    @review.user = current_user
+    if @reservation.save
+        redirect_to offer_path(@reservation.offer), notice: "review created"
+    else
+      render "pages#dashboard", alert: "try again dude !"
+    end 
+  end 
+    private
+  def review_params
+      params.require(:review).permit(:rating, :content)
+  end 
+  
+  
 end
