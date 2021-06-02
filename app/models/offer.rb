d243e424c712c23cd0270c5067237cdbd6b667f3
pierre averous <pierreaverous42@gmail.com>
2021-06-02 13:28:23 +0200

    button and dropdown list

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,7 @@ class Offer < ApplicationRecord
   belongs_to :user
   has_many :reservations
   has_many :reviews, through: :reservations
-  validates :disponibility, presence: true
+  validates :disponibility,inclusion: {in: ["Week end", "Semaine", "AfterWork", "Every day"]}, presence: true
   validates :localisation, presence: true 
+  
 end
