eed3d1750a16ab3189f98cf261a660f0d0c826b7
florent astier <florenttier@gmail.com>
2021-06-03 22:23:09 +0200

    changed the js to be better and fix little bug

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -6,6 +6,7 @@ class Offer < ApplicationRecord
   has_many :reviews, through: :reservations
   validates :disponibility, presence: true
   validates :localisation, presence: true
+  validates :price, presence: true
   validates :disponibility, inclusion: { in: ["Weekend", "Week", "Afterwork", "Every Day"], message: "Please select one of the disponibility" }
   include PgSearch::Model
   pg_search_scope :search_by_localisation,
